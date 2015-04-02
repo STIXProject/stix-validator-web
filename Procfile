c2f4a97c8053984d20d67862ecdede01d4eb7817
John Wunder <jwunder@mitre.org>
2015-04-02 13:11:32 -0400

    initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn web-ui:app --log-file=-
--- web-ui.py
+++ web-ui.py
@@ -1,14 +1,28 @@
-from flask import Flask, request
+from flask import Flask, request, Response
 import sdv
 import StringIO
+import urllib2
 
 app = Flask(__name__)
 
-@app.route('/validate', methods=['POST'])
+@app.route('/validate', methods=['POST', 'GET'])
 def validate_ws():
-    data = request.get_data()
+    post_data = request.get_data()
+    url = request.args.get('url')
+
+    if url:
+        print url
+        proxies = {'http': 'http://gatekeeper.mitre.org:80', 'https': 'http://gatekeeper.mitre.org:80'}
+        return get_results(urllib2.urlopen(url).read())
+    elif len(post_data) > 0:
+        return get_results(post_data)
+    else:
+        return Response('{"result": "error", "message": "could not find xml"}\n', mimetype='application/json')
+
+
+def get_results(data):
     results = sdv.validate_xml(StringIO.StringIO(data))
-    return Response(results.as_json(), mimetype='application/json')
+    return Response(results.as_json() + "\n", mimetype='application/json')
 
 if __name__ == "__main__":
-    app.run('0.0.0.0', 5000, True)
+    app.run('localhost', 5000, True)
